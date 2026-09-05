49d86d8c696086553b5cdc68120d58c7bd074b42
Izabela K <aizeilla12344@gmail.com>
2026-09-05 23:43:02 +0200

    feat: add session print out

--- src/main/java/com/example/springdemo/Controller.java
+++ src/main/java/com/example/springdemo/Controller.java
@@ -1,5 +1,6 @@
 package com.example.springdemo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/")
-    public String greet(){
-        return "Test message";
+    public String greet(HttpServletRequest request){
+        return "Test message, your session id: " + request.getSession().getId();
     }
 }
